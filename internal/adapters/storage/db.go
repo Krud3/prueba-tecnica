bdebe2ae3a5ce09be506810d40c883602cf175b6
krud3 <juan.sebastian.molina@correounivalle.edu.co>
2025-06-28 15:48:34 -0500

    conexion and testing pseudo-migration

---
+++ internal/adapters/storage/db.go
@@ -0,0 +1,34 @@
+// internal/adapters/storage/db.go
+package storage
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/krud3/prueba-tecnica/internal/core/domain"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func NewGormDB() (*gorm.DB, error) {
+	godotenv.Load()
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&domain.Customer{}, &domain.WorkOrder{})
+	return db, err
+}

a0724de90a89b5fe337dee3dd28c9fc42bebaa8d
krud3 <juan.sebastian.molina@correounivalle.edu.co>
2025-06-29 09:26:58 -0500

    created first interface named internal/core/ports/ports.go

---
+++ internal/core/ports/ports.go
@@ -0,0 +1,31 @@
+// internal/core/ports/ports.go
+
+package ports
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/krud3/prueba-tecnica/internal/core/domain"
+)
+
+type WorkOrderFilters struct {
+	Since  *time.Time
+	Until  *time.Time
+	Status *domain.Status
+}
+
+type CustomerRepository interface {
+	Create(ctx context.Context, customer domain.Customer) error
+	FindByID(ctx context.Context, id uuid.UUID) (*domain.Customer, error)
+	GetActive(ctx context.Context) ([]domain.Customer, error)
+	Update(ctx context.Context, customer domain.Customer) error
+}
+
+type WorkOrderRepository interface {
+	Create(ctx context.Context, workOrder domain.WorkOrder) error
+	FindByID(ctx context.Context, id uuid.UUID) (*domain.WorkOrder, error)
+	FindByDateRange(ctx context.Context, filters WorkOrderFilters) ([]domain.WorkOrder, error)
+	Update(ctx context.Context, workOrder domain.WorkOrder) error
+}

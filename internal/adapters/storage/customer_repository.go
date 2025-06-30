e5cccf22435971864f7e8f3e6ccaed98803c76e1
krud3 <juan.sebastian.molina@correounivalle.edu.co>
2025-06-29 23:37:53 -0500

    implemented gorm customer repository

---
+++ internal/adapters/storage/customer_repository.go
@@ -0,0 +1,74 @@
+// internal/adapters/storage/customer_repository.go
+
+package storage
+
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/krud3/prueba-tecnica/internal/core/domain"
+	"github.com/krud3/prueba-tecnica/internal/core/ports"
+	"gorm.io/gorm"
+)
+
+var (
+	ErrNoCID = errors.New("no se encontró ID asociada al customer")
+)
+
+type gormCustomerRepository struct {
+	db *gorm.DB
+}
+
+func NewGormCustomerRepository(db *gorm.DB) ports.CustomerRepository {
+	return &gormCustomerRepository{db: db}
+}
+
+func (r *gormCustomerRepository) Create(ctx context.Context, customer domain.Customer) error {
+	//uuid if not exist
+	if customer.ID == uuid.Nil {
+		customer.ID = uuid.New()
+	}
+	// create customer
+	result := r.db.WithContext(ctx).Create(&customer)
+
+	// if any error return else nil
+	return result.Error
+}
+
+func (r *gormCustomerRepository) FindByID(ctx context.Context, id uuid.UUID) (*domain.Customer, error) {
+	// to storage customer
+	var customer domain.Customer
+
+	// assings customer by pointer
+	result := r.db.WithContext(ctx).First(&customer, "id = ?", id)
+	if result.Error != nil {
+		// if not found
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		// if error
+		return nil, result.Error
+	}
+	// founded
+	return &customer, nil
+}
+
+func (r *gormCustomerRepository) GetActive(ctx context.Context) ([]domain.Customer, error) {
+	// to storages customers
+	var customers []domain.Customer
+
+	// search is active and stores it catch error if error
+	err := r.db.WithContext(ctx).Where("is_active = ?", true).Find(&customers).Error
+
+	// results
+	return customers, err
+}
+
+func (r *gormCustomerRepository) Update(ctx context.Context, customer domain.Customer) error {
+	if customer.ID == uuid.Nil {
+		return ErrNoCID
+	} else {
+		return r.db.WithContext(ctx).Save(customer).Error
+	}
+}
